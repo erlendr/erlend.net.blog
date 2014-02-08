943d099fbf6ac2ed26d7290156db52a1fe8892ea
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-02-08 21:48:34 +0100

    initial commit of node rewrite

--- Erlend.Net.Blog/Erlend.Net.Blog/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Erlend.Net.Blog.MvcApplication" Language="C#" %>
---
+++ Erlend.Net.Blog/routes/archives.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET archives page
+ */
+
+exports.index = function(req, res){
+  res.render('contact', { title: 'Archives' });
+};
